4916226dcd1a5e9d8568601b1adbaacdf81d8440
abhi62 <abhi.leon.93@gmail.com>
2022-01-27 19:24:51 +0530

    Productioin build: container webpack for prod done

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,20 +1,5 @@
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.m?.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-react', '@babel/preset-env'],
-//             plugins: ['@babel/plugin-transform-runtime'],
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -31,4 +16,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
