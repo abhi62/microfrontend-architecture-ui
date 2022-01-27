d0e786385b9237d8131400d3e778a7a3089aa31d
abhi62 <abhi.leon.93@gmail.com>
2022-01-27 18:56:47 +0530

    Microfrontend base added

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+function App() {
+  return (
+    <div>
+      <h1>Hi there! i am from CONTAINER (React app)</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+}
+
+export default App;
